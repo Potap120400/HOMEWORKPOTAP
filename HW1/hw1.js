9e2e7aa913c11a50ec10d2a903a5154dee176a74
Vlad Potapenko <51811015+Potap120400@users.noreply.github.com>
2021-09-23 17:19:30 +0300

    add

---
+++ HW1/hw1.js
@@ -0,0 +1,42 @@
+let chiken = 15.678;
+let beef = 123.965;
+let bacon = 90.2345;
+
+
+console.log(Math.max(chiken,beef,bacon)); 
+console.log(Math.min(chiken,beef,bacon));   
+
+let sum = (chiken+beef+bacon);
+console.log(sum);
+
+console.log(Math.floor(sum));
+console.log(Math.ceil(sum /100)*100);
+
+console.log(Math.floor(sum) %2 == 0);
+console.log(500-sum);
+console.log((sum/3).toFixed(2));
+
+let sale = Math.random()*101;
+console.log(sale)
+
+let priceSale = (sum)-((sum*sale)/100);
+console.log((priceSale).toFixed(2));
+
+let navar = (sum/2)-((sum*sale)/100);
+console.log((navar).toFixed(2));
+
+
+
+
+document.writeln(`<i>Максимальна ціна = ${Math.max(chiken,beef,bacon)} <br>
+Мінімальна ціна = ${Math.min(chiken,beef,bacon)} <br>
+Загальна  вартість = ${sum} <br>
+Сума без копійок = ${Math.floor(sum)} <br>
+Сума округлена до сотень = ${Math.ceil(sum /100)*100} <br>
+Парне чи не парне = ${Math.floor(sum) %2 == 0} <br>
+Решта від 500 = ${500-sum} <br>
+Cереднє значення цін, округлене до другого знаку після коми = ${(sum/3).toFixed(2)} <br>
+Випадкова знижка = ${sale} <br>
+Сума зі скиидкою = ${(priceSale).toFixed(2)} <br>
+Чистий прибуток = ${(navar).toFixed(2)} <br>
+`);
