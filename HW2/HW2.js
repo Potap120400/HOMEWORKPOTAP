3b285cf56f81233c08276a5e4a096b34f0a780d8
Vlad Potapenko <51811015+Potap120400@users.noreply.github.com>
2021-09-28 16:02:20 +0300

    hw

--- HW2/HW2.js
+++ HW2/HW2.js
@@ -1,16 +1,27 @@
-let N = parseInt(prompt('Введіть значення  N:'));
-let M = parseInt(prompt('Введіть значення M:'));
 
-let skipNumber = confirm('Пропустити парні числа?');
-let result = 0;
+let N = '';
+do {
+    N = prompt('Введіть значення N:','').trim();
+} while (N === '' || isNaN (N) || N === null);
+const parsedN = parseInt(N);
+console.log (parsedN);
 
-for (let i = N; i <= M; i++) {
-    if (skipNumber && !(i % 2)) {
-        continue;
+let M = '';
+do {
+    M = prompt('Введіть значення M:','').trim();
+} while (M === '' || isNaN (M) || M === null);
+const parsedM = parseInt(M);
+console.log (parsedM);
+
+const skipEven = confirm('Пропустити парні числа?');
+console.log(skipEven);
+let sum = 0;
+        for (i = parsedN; i <= parsedM; i ++) {
+            if (skipEven && i % 2 === 0) {
+                continue;
+            }else {
+                sum += i;    
     }
-    result += i;
 }
-
-alert(`Result: ${result}`);
-
-document.writeln(`Сумма чисел: ${result}`);
+console.log(sum);
+alert (`Сума показників: ${sum}`);
