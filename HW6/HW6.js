7cfbecdc99c3547d20a2d0ef4be5d93f77394468
Vlad Potapenko <51811015+Potap120400@users.noreply.github.com>
2021-10-16 16:39:45 +0300

    hw

--- HW6/HW6.js
+++ HW6/HW6.js
@@ -1,8 +1,13 @@
 //1
 const getRandomArray = (length, min, max) => {
-    const arr = [...Array(length)].map((item) => Math.floor(Math.random() * (max - min + 1)) + min);
-    return arr;
-};
+	const randomArray = [];
+
+	for (let i = 0; i < length; i++) {
+		randomArray.push(Math.round(Math.random() * (max - min) + min));
+	}
+
+	return randomArray;
+}
 console.log(getRandomArray(15, 1, 100));
 
 
@@ -18,7 +23,7 @@ const getAverage = (...numbers) => {
 
 
 //5
-const filterEvenNumbers = (...numbers) => numbers.filter((number) => (number % 2));
+const filterEvenNumbers = (...numbers) => numbers.filter((number) => number % 2);
 console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
 //6
 const countPositiveNumbers = (...numbers) => {
@@ -31,12 +36,15 @@ console.log(getDividedByFive(10, 5, 43, 45, 55, 63, 2, 22));
 
 //9
 const divideByThree = (word) => {
-    word = word.toLowerCase().split('');
-    const newWord = [];
-    let i = 0; 
-    while (i < word.length) {
-      newWord.push(word.slice(i, i += 3).join(''));
+    if (word.length < 3) {
+        return word;
     }
-    return newWord;
+    const wordLowCase = word.toLowerCase().replaceAll(" ", "");
+    let arr = [];
+    for (let i = 0; i < wordLowCase.length; i += 3) {
+        let dividedWord = wordLowCase.slice(i, i + 3);
+        arr.push(dividedWord);
+    }
+    return arr;
 };
 console.log(divideByThree("live"));
